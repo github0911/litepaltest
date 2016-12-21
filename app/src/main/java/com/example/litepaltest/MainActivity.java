bc654fb6941806e73a08b432596d6963e7a8a50b
zhangmingming <open_m@163.com>
2016-12-21 14:20:18 +0800

    添加增删改查

--- app/src/main/java/com/example/litepaltest/MainActivity.java
+++ app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -5,12 +5,17 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.litepaltest.base.BasicActivity;
 import com.example.litepaltest.bean.Book;
+import com.google.gson.Gson;
 
+import org.litepal.crud.DataSupport;
 import org.litepal.tablemanager.Connector;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -27,12 +32,29 @@ public class MainActivity extends BasicActivity {
     EditText price;
     @BindView(R.id.save)
     Button save;
+    @BindView(R.id.update)
+    Button update;
     @BindView(R.id.create_database)
     Button createData;
+    @BindView(R.id.result)
+    TextView result;
+    @BindView(R.id.query)
+    Button query;
+    @BindView(R.id.queryAll)
+    Button queryAll;
+
+    Gson gson;
+    @BindView(R.id.delete_id)
+    EditText deleteId;
+    @BindView(R.id.delete)
+    Button delete;
+    @BindView(R.id.delete_all)
+    Button deleteAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        gson = new Gson();
     }
 
     @Override
@@ -59,6 +81,57 @@ public class MainActivity extends BasicActivity {
         } else {
             Toast.makeText(this, "save failed", Toast.LENGTH_SHORT).show();
             Log.i(TAG, "save: success");
+        }
+    }
+
+    @OnClick(R.id.query)
+    public void query() {
+        Book book = DataSupport.findFirst(Book.class);
+        result.setText(gson.toJson(book));
+    }
+
+    @OnClick(R.id.queryAll)
+    protected void queryAll() {
+        List<Book> allBooks = DataSupport.findAll(Book.class);
+        result.setText(gson.toJson(allBooks));
+    }
+
+    @OnClick(R.id.delete)
+    public void delete(){
+        if (TextUtils.isEmpty(deleteId.getText().toString())) {
+            Toast.makeText(this, "deleteId is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        int count = DataSupport.deleteAll(Book.class, "id = ?", deleteId.getText().toString());
+        if (count > 0) {
+            Toast.makeText(this, "delete success", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "delete failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick(R.id.delete_all)
+    public void deleteAll(){
+        int count = DataSupport.deleteAll(Book.class);
+        if (count > 0) {
+            Toast.makeText(this, "delete success", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "delete failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick(R.id.update)
+    public void update(){
+        Book book = new Book();
+        book.setAuthor(author.getText().toString());
+        book.setName(name.getText().toString());
+        book.setPrice(Double.valueOf(price.getText().toString()));
+        int count = book.update(15);
+
+        if (count > 0) {
+            Toast.makeText(this, "update success", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "update failed", Toast.LENGTH_SHORT).show();
         }
     }
 }
